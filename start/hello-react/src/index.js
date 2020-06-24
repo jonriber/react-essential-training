d41c115a89d07240d8c6035d7b696e063c5f7d3b
jonriber <49167364+jonriber@users.noreply.github.com>
2020-06-24 14:57:32 -0300

    second module "React Components"

--- start/hello-react/src/index.js
+++ start/hello-react/src/index.js
@@ -6,8 +6,54 @@ let city = {
     name: "Madrid",
     country:'Spain',
 };
+
+// function Hello(props) { //This is components, captalized
+//   console.log(props);
+//   return (
+//     <div>
+//       <h1>Welcome to {props.library}!!</h1>
+//       <p>{props.message}</p>
+//       <p>{props.number}</p>
+//     </div>
+    
+//   )};
+// function Lake({name}) {
+//   return (
+//   <h1>{name}</h1>
+//   )
+// }
+const lakeList = [
+  {id:"1",name:"Echo",trailhead:"Echo"},
+  {id:"2",name:"Maud",trailhead:"Wrigts"},
+  {id:"3",name:"Velma",trailhead:"Bayview"},
+];
+
+function App({lakes}) {
+  return (
+    <div>
+      {lakes.map(lake => (
+        <div key={lake.id}>
+          <h2>{lake.name}</h2>
+          <p>Accessed by: {lake.trailhead}</p>
+        </div>
+      ))};
+    </div>
+  );
+  
+};
+
+function SkiResort(){
+  return(
+    <div>
+      <h1>Visit JacksonHole Mountain Resort</h1>
+    </div>
+  )
+};
+
 ReactDOM.render(
-  <h1 id='heading' className='cool-text'>Hello from {city.name} in {city.country} </h1>,
+  <App lakes = {lakeList}/>,
+  // <Hello library="React" message ="Have fun" number = {3}/>,
+  // <h1 id='heading' className='cool-text'>Hello from {city.name} in {city.country} </h1>,
   document.getElementById('root')
 );
 
