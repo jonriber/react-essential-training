e7fec0d321a38091dc5517c4dbf859c21531d5c6
jonriber <49167364+jonriber@users.noreply.github.com>
2020-06-26 15:18:56 -0300

    github data

--- start/hello-react/src/index.js
+++ start/hello-react/src/index.js
@@ -1,11 +1,11 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-let city = {
-    name: "Madrid",
-    country:'Spain',
-};
+// let city = {
+//     name: "Madrid",
+//     country:'Spain',
+// };
 
 // function Hello(props) { //This is components, captalized
 //   console.log(props);
@@ -22,36 +22,48 @@ let city = {
 //   <h1>{name}</h1>
 //   )
 // }
-const lakeList = [
-  {id:"1",name:"Echo",trailhead:"Echo"},
-  {id:"2",name:"Maud",trailhead:"Wrigts"},
-  {id:"3",name:"Velma",trailhead:"Bayview"},
-];
 
-function App({lakes}) {
-  return (
-    <div>
-      {lakes.map(lake => (
-        <div key={lake.id}>
-          <h2>{lake.name}</h2>
-          <p>Accessed by: {lake.trailhead}</p>
-        </div>
-      ))};
-    </div>
-  );
+// const lakeList = [
+//   {id:"1",name:"Echo",trailhead:"Echo"},
+//   {id:"2",name:"Maud",trailhead:"Wrigts"},
+//   {id:"3",name:"Velma",trailhead:"Bayview"},
+// ];
+// const Lake = () => {
+//   <h1>Lake!</h1>
+// };
+
+// const SkiResort = () => {
+//   <h1>Ski Resort!</h1>
+// };
+function GitHubUser({login}) {
+  const [data,setData] = useState(null);
+  useEffect(() => {
+    fetch(`https://api.github.com/users/${login}`)
+    .then(res => res.json())
+    .then(setData)
+    .catch(console.error);
+  },[]);
+  if (data) {
+    return (
+      <div>
+        <h1>{data.login}</h1>
+        <img src={data.avatar_url} width={100}></img>
+      </div>
+    );
+  }
+  return null;
+}
+
+//this is App component
+function App() {
   
-};
-
-function SkiResort(){
-  return(
-    <div>
-      <h1>Visit JacksonHole Mountain Resort</h1>
-    </div>
-  )
+  return ( 
+    <GitHubUser login='jonriber'/>
+  );
 };
 
 ReactDOM.render(
-  <App lakes = {lakeList}/>,
+  <App/>, //using component App
   // <Hello library="React" message ="Have fun" number = {3}/>,
   // <h1 id='heading' className='cool-text'>Hello from {city.name} in {city.country} </h1>,
   document.getElementById('root')
